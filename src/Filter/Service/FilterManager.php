9b614928ddbecf7341ff1fbaa67f2a1cf93b2d54
Ian Littman <iansltx@gmail.com>
2017-04-08 01:04:57 -0500

    Slight cleanup on managers, allow default value for OrderByManager

--- src/Filter/Service/FilterManager.php
+++ src/Filter/Service/FilterManager.php
@@ -8,9 +8,7 @@ use jackdpeterson\Doctrine\QueryBuilder\Filter\FilterInterface;
 
 class FilterManager
 {
-
     protected $filters;
-
 
     public function __construct(array $filters)
     {
@@ -27,9 +25,10 @@ class FilterManager
             throw new \RuntimeException("QB Filter Not found: " . $filterName);
         }
 
-        if (!is_a($this->filters[$filterName], FilterInterface::class, true)) {
+        if (!is_subclass_of($this->filters[$filterName], FilterInterface::class, true)) {
             throw new \RuntimeException('Provided class name is not a FilterInterface');
         }
+
         $filter = new $this->filters[$filterName]([$this]);
 
         if (!$filter instanceof FilterInterface) {
@@ -40,12 +39,6 @@ class FilterManager
         return $filter;
     }
 
-
-    /**
-     * @var string
-     */
-    protected $instanceOf = FilterInterface::class;
-
     public function filter(QueryBuilder $queryBuilder, $metadata, $filters)
     {
         foreach ($filters as $option) {
@@ -53,8 +46,7 @@ class FilterManager
                 throw new RuntimeException('Array element "type" is required for all filters');
             }
 
-            $filter = $this->get(strtolower($option['type']), [$this]);
-            $filter->filter($queryBuilder, $metadata, $option);
+            $this->get(strtolower($option['type']))->filter($queryBuilder, $metadata, $option);
         }
     }
 }
--- src/OrderBy/Service/OrderByManager.php
+++ src/OrderBy/Service/OrderByManager.php
@@ -8,15 +8,12 @@ use jackdpeterson\Doctrine\QueryBuilder\OrderBy\OrderByInterface;
 
 class OrderByManager
 {
-
     protected $orderByHandlers;
-
 
     public function __construct(array $orderByHandlers)
     {
         $this->orderByHandlers = $orderByHandlers;
     }
-
 
     /**
      * @param string $orderByHandlerName
@@ -24,7 +21,7 @@ class OrderByManager
      */
     protected function get(string $orderByHandlerName): OrderByInterface
     {
-        if (!array_key_exists($orderByHandlerName, $this->orderBy)) {
+        if (!array_key_exists($orderByHandlerName, $this->orderByHandlers)) {
             throw new \RuntimeException("QB Order By Handler Not found: " . $orderByHandlerName);
         }
 
@@ -32,10 +29,11 @@ class OrderByManager
             throw new \RuntimeException("QB Order By Handler Class Not found in autoloader: " . $orderByHandlerName);
         }
 
-        if (!is_a($this->orderByHandlers[$orderByHandlerName], OrderByInterface::class, true)) {
+        if (!is_subclass_of($this->orderByHandlers[$orderByHandlerName], OrderByInterface::class, true)) {
             throw new \RuntimeException('Provided class name is not an OrderByInterface');
         }
-        $orderByHandler = new $this->filters[$filterName]([$this]);
+
+        $orderByHandler = new $this->orderByHandlers[$orderByHandlerName]([$this]);
 
         if (!$orderByHandler instanceof OrderByInterface) {
             throw new \RuntimeException(sprintf('Requested %1$s however, %1$s is not an instance of OrderByInterface!',
@@ -45,21 +43,19 @@ class OrderByManager
         return $this->orderByHandlers[$orderByHandlerName];
     }
 
-    /**
-     * @var string
-     */
-    protected $instanceOf = OrderByInterface::class;
-
     public function orderBy(QueryBuilder $queryBuilder, $metadata, $orderBy)
     {
         foreach ($orderBy as $option) {
+            if (count($this->orderByHandlers) === 1) {
+                $option['type'] = key($this->orderByHandlers);
+            }
+
             if (empty($option['type'])) {
                 throw new RuntimeException('Array element "type" is required for all orderby directives');
             }
 
-            $orderByHandler = $this->get(strtolower($option['type']), [$this]);
+            $orderByHandler = $this->get(strtolower($option['type']));
             $orderByHandler->orderBy($queryBuilder, $metadata, $option);
         }
     }
-
 }
